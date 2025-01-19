92e2bedc4e40b3ca6a45580d8b182e6c856f2cb9
sw-99 <jswpm0359@gmail.com>
2025-01-20 01:30:06 +0900

    feat(B/config): WebConfig 추가 및 CORS 설정 적용

---
+++ simpleject-backend/src/main/java/com/simpleject/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.simpleject.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
