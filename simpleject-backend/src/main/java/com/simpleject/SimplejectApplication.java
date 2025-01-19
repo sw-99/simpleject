231b1426c78eaea5c196f9b1226b81b94eb05b5e
sw-99 <jswpm0359@gmail.com>
2025-01-19 15:51:53 +0900

    commit init

---
+++ simpleject-backend/src/main/java/com/simpleject/SimplejectApplication.java
@@ -0,0 +1,13 @@
+package com.simpleject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplejectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplejectApplication.class, args);
+	}
+
+}
